de642fa104c033f99ab4a9bd070b97d9bb90e0fe
haien <s18b701@shohoku.ac.jp>
2019-12-19 14:06:02 +0900

    Initial commit

---
+++ app/src/main/java/ishiicai/shohoku/ac/jp/yami_tweet_mixir/MainActivity.java
@@ -0,0 +1,14 @@
+package ishiicai.shohoku.ac.jp.yami_tweet_mixir;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
