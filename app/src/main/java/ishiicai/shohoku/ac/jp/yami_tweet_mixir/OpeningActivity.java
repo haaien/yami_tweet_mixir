91b7e53fac55e71beefb47b54124df4c7cc03617
Haya <s18b716@shohoku.ac.jp>
2020-01-09 14:29:45 +0900

    最低限のTweetActivityレイアウトの作成（後で調整して他のレイアウトにもコピペします） 石井くんガンバ！

--- app/src/main/java/ishiicai/shohoku/ac/jp/yami_tweet_mixir/OpeningActivity.java
+++ app/src/main/java/ishiicai/shohoku/ac/jp/yami_tweet_mixir/OpeningActivity.java
@@ -1,4 +1,25 @@
 package ishiicai.shohoku.ac.jp.yami_tweet_mixir;
 
-public class OpeningActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OpeningActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.opening_layout);
+    }
+
+    //画面を押し(離し)たときにミキサー画面へ遷移する
+    @Override
+    public boolean onTouchEvent(MotionEvent T_Event) {
+        if (T_Event.getAction() == MotionEvent.ACTION_UP) {
+            Intent intent = new Intent(OpeningActivity.this, TweetActivity_Empty.class);
+            startActivity(intent);
+        }
+        return true;
+    }
 }
