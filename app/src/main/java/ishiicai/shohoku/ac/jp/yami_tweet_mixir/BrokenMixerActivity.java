b0b69ee04faea595a35e089abd2bbaef5f681a68
username(new) <useremail(new)>
2020-01-15 06:07:08 +0900

    あああああああああああああああああああああああああああああああああ

--- app/src/main/java/ishiicai/shohoku/ac/jp/yami_tweet_mixir/BrokenMixerActivity.java
+++ app/src/main/java/ishiicai/shohoku/ac/jp/yami_tweet_mixir/BrokenMixerActivity.java
@@ -1,4 +1,44 @@
 package ishiicai.shohoku.ac.jp.yami_tweet_mixir;
 
-public class BrokenMixerActivity {
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BrokenMixerActivity extends AppCompatActivity {
+
+    private VideoView mVideoView;
+
+
+
+    protected  void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tmp_stab_layout);
+
+        mVideoView = (VideoView)findViewById(R.id.videoView6);
+        mVideoView.setVideoURI(Uri.parse("android.resource://" + this.getPackageName() + "/" +R.raw.burn_shock));
+        //動画の再生開始
+        mVideoView.start();
+
+        HandlerThread handlerThread = new HandlerThread("foo");
+        handlerThread.start();
+        new Handler(handlerThread.getLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // 3秒後に表示
+                //インテントに、この画面と、遷移する別の画面を指定する
+                Intent intent = new Intent(getApplication(), MainActivity.class);
+                //インテントで指定した別の画面に遷移する
+                startActivity(intent);
+            }
+        }, 10000);
+
+
+
+    }
 }
--- app/src/main/java/ishiicai/shohoku/ac/jp/yami_tweet_mixir/MainActivity.java
+++ app/src/main/java/ishiicai/shohoku/ac/jp/yami_tweet_mixir/MainActivity.java
@@ -1,14 +1,28 @@
 package ishiicai.shohoku.ac.jp.yami_tweet_mixir;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.opening_layout);
+
+        Button button1 = (Button) findViewById(R.id.opening);     //画面遷移ボタン
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //インテントに、この画面と、遷移する別の画面を指定する
+                Intent intent = new Intent(getApplication(), TweetActivity_TwentyPer.class);
+                //インテントで指定した別の画面に遷移する
+                startActivity(intent);
+            }
+        });
     }
 }
